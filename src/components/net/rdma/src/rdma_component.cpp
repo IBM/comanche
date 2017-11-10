1de291768bc12b837ccaa66cd173b3f756ec261f
Daniel Waddington <daniel.waddington@ibm.com>
2017-11-10 14:48:26 -0800

    very basic rdma component

--- src/components/net/rdma/src/rdma_component.cpp
+++ src/components/net/rdma/src/rdma_component.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "rdma_component.h"
 
-Rdma_component::Rdma_component(const std::string& device_name)
+Rdma_component::Rdma_component(const std::string& device_name) : _device_name(device_name)
 {
 }
 
@@ -11,7 +11,18 @@ Rdma_component::~Rdma_component()
 
 status_t Rdma_component::connect(const std::string& peer_name, int port)
 {
-  return E_FAIL;
+  if(_device_name == "any")
+    return _transport.connect(NULL, peer_name.c_str(), port);
+  else
+    return _transport.connect(_device_name.c_str(), peer_name.c_str(), port);
+}
+
+status_t Rdma_component::wait_for_connect(int port)
+{
+  if(_device_name == "any")
+    return _transport.wait_for_connect(NULL, port);
+  else
+    return _transport.wait_for_connect(_device_name.c_str(), port);
 }
 
 status_t Rdma_component::disconnect()
@@ -19,6 +30,34 @@ status_t Rdma_component::disconnect()
   return E_FAIL;
 }
 
+
+struct ibv_mr * Rdma_component::register_memory(void * contig_addr, size_t size)
+{
+  return _transport.register_memory(contig_addr,size);
+}
+
+uint64_t Rdma_component::post_send(struct ibv_mr * mr0, struct ibv_mr * extra_mr)
+{
+  uint64_t gwid = next_gwid();
+  if(_transport.post_send(gwid, mr0, extra_mr) != S_OK)
+    throw General_exception("rdma transport post_send failed");
+
+  return gwid;
+}
+
+uint64_t Rdma_component::post_recv(struct ibv_mr * mr0)
+{
+  uint64_t gwid = next_gwid();
+  if(_transport.post_recv(gwid, mr0) != S_OK)
+    throw General_exception("rdma transport post_recv failed");
+
+  return gwid;
+}
+
+int Rdma_component::poll_completions(std::function<void(uint64_t)> completion_func)
+{
+  return _transport.poll_completions(completion_func);
+}
 
 /** 
  * Factory entry point
--- src/components/net/rdma/unit_test/test1.cpp
+++ src/components/net/rdma/unit_test/test1.cpp
@@ -1,14 +1,17 @@
 /* note: we do not include component source, only the API definition */
 #include <gtest/gtest.h>
 #include <api/components.h>
-#include <api/sample_itf.h>
+#include <api/rdma_itf.h>
+#include <common/utils.h>
+#include <core/dpdk.h>
+#include <core/physical_memory.h>
 
 using namespace Component;
 
 namespace {
 
 // The fixture for testing class Foo.
-class Sample_test : public ::testing::Test {
+class Rdma_test : public ::testing::Test {
 
  protected:
 
@@ -26,48 +29,101 @@ class Sample_test : public ::testing::Test {
   }
   
   // Objects declared here can be used by all tests in the test case
-  static Component::ISample * _sample;
+  static Component::IRdma * _rdma;
 };
 
-Component::ISample * Sample_test::_sample;
+bool client;
+Component::IRdma * Rdma_test::_rdma;
 
+TEST_F(Rdma_test,DpdkInit)
+{
+  if(client)
+    DPDK::eal_init(32, 0, false); /* slave */
+  else
+    DPDK::eal_init(32, 0, true); /* master */
+}
 
-
-TEST_F(Sample_test, Instantiate)
+TEST_F(Rdma_test, Instantiate)
 {
   /* create object instance through factory */
-  Component::IBase * comp = Component::load_component("libsample-component.so",
-                                                      Component::sample_component_factory);
+  Component::IBase * comp = Component::load_component("libcomanche-rdma.so",
+                                                      Component::net_rdma_factory);
 
   ASSERT_TRUE(comp);
-  ISample_factory * fact = (ISample_factory *) comp->query_interface(ISample_factory::iid());
+  IRdma_factory * fact = (IRdma_factory *) comp->query_interface(IRdma_factory::iid());
 
-  _sample = fact->open("Billybob");
+  _rdma = fact->create("any");
   
   fact->release_ref();
 }
 
-TEST_F(Sample_test, Invoke)
-{
-  ASSERT_TRUE(_sample);
+int port = 18515;
+std::string remote_host;
 
-  /* invoke instance */
-  _sample->say_hello();
+TEST_F(Rdma_test, Connect)
+{
+  if(client) {
+    ASSERT_TRUE(_rdma->connect(remote_host.c_str(), port) == S_OK);
+  }
+  else {
+    ASSERT_TRUE(_rdma->wait_for_connect(port) == S_OK);
+  }
+  PLOG("Connected OK!");
 }
 
-TEST_F(Sample_test, Release)
+TEST_F(Rdma_test, ExchangeData)
 {
-  ASSERT_TRUE(_sample);
+  Core::Physical_memory mem;
+  Component::io_buffer_t iob = mem.allocate_io_buffer(KB(4),
+                                                      KB(4),
+                                                      Component::NUMA_NODE_ANY);
+  unsigned ITERATIONS = 100;
 
-  /* release instance */
-  _sample->release_ref();
+  char  * msg = (char *) mem.virt_addr(iob);
+  auto rdma_buffer = _rdma->register_memory(mem.virt_addr(iob),KB(4));
+
+  for(unsigned i=0;i<ITERATIONS;i++) {
+    if(client) {
+      sprintf(msg, "Hello %u !!", i);
+      _rdma->post_send(rdma_buffer);
+      int n_complete = 0;
+      while((n_complete = _rdma->poll_completions()) == 0);
+      ASSERT_TRUE(n_complete == 1);
+    }
+    else {
+      _rdma->post_recv(rdma_buffer);
+      int n_complete = 0;
+      while((n_complete = _rdma->poll_completions()) == 0);
+      ASSERT_TRUE(n_complete == 1);
+      PLOG("received: %s", msg);
+    }
+  }
+  
+  mem.free_io_buffer(iob);
 }
+
+
+TEST_F(Rdma_test, Cleanup)
+{
+  _rdma->release_ref();
+}
+
 
 
 } // namespace
 
 int main(int argc, char **argv) {
-  
+
+  if(argc < 2) {
+    PINF("rdma-test1 [client ipaddr| server]");
+    return -1;
+  }
+
+  client = (strcmp(argv[1],"client")==0);
+  if(client) {
+    assert(argc==3);
+    remote_host = argv[2];
+  }  
   ::testing::InitGoogleTest(&argc, argv);
   auto r = RUN_ALL_TESTS();
 
