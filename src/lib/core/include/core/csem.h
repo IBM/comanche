06d7cdaacfee052ace4205f4f2f21573785c7c5a
Daniel Waddington <danielwaddington@ibm.com>
2018-01-08 07:56:53 -0800

    wip

---
+++ src/lib/core/include/core/csem.h
@@ -0,0 +1,64 @@
+/*
+   Copyright [2017] [IBM Corporation]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
+/* 
+ * Authors: 
+ * 
+ * Daniel G. Waddington (daniel.waddington@ibm.com)
+ *
+ */
+
+#ifndef __COMANCHE_SEMAPHORE_H__
+#define __COMANCHE_SEMAPHORE_H__
+
+#include <mutex>
+#include <condition_variable>
+
+namespace Core
+{
+
+class Semaphore {
+public:
+  Semaphore (int count_ = 0)
+    : count(count_) {}
+
+  inline void post()
+  {
+    std::unique_lock<std::mutex> lock(mtx);
+    count++;
+    cv.notify_one();
+  }
+
+  inline void wait()
+  {
+    std::unique_lock<std::mutex> lock(mtx);
+
+    while(count == 0){
+      cv.wait(lock);
+    }
+    count--;
+  }
+
+private:
+  std::mutex mtx;
+  std::condition_variable cv;
+  int count;
+};
+
+}
+
+#endif
