f1826d0fc498ee4c04b794d36d0a8c8a5b050ddc
Daniel Waddington <daniel.waddington@ibm.com>
2018-03-02 11:58:16 -0800

    new cmake include method

--- autogen.py
+++ autogen.py
@@ -2,14 +2,20 @@
 import os
 
 comancheRoot = os.getcwd()
-rootDir = './src'
-for dirName, subdirList, fileList in os.walk(rootDir):
-    print('Found directory: %s' % dirName)
-    for fname in os.listdir('.'):
-        if fname == 'CMakeLists.txt':
-            file = open(dirName + "/.cmakeinclude","w+")
-            file.truncate(0)
-            file.write("set(ENV{COMANCHE_HOME} " + comancheRoot + ")\n")
-            file.write("include($ENV{COMANCHE_HOME}/mk/common.cmake)\n")
-            break
+
+def gen(rootDir):
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        print('Found directory: %s' % dirName)
+        for fname in os.listdir('.'):
+            if fname == 'CMakeLists.txt':
+                file = open(dirName + "/.cmakeinclude","w+")
+                file.truncate(0)
+                file.write("set(ENV{COMANCHE_HOME} " + comancheRoot + ")\n")
+                file.write("include($ENV{COMANCHE_HOME}/mk/common.cmake)\n")
+                break
+
+gen('./src')
+gen('./apps')
+
+
 
