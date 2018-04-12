798baf90b7de7c7c4fe49550cbeea5eb9241ab15
Daniel Waddington <danielwaddington@ibm.com>
2018-04-12 06:27:17 -0700

    missing file

---
+++ src/components/examples/python-sample/pysample.py
@@ -0,0 +1,11 @@
+# python class
+import sys
+
+class Sample:
+    def __init__(self):
+        print("Python Sample component instantiated.")
+
+    def say_hello(self):
+        print("Say hello from Python my friend! :" + str(sys.version))
+
+
