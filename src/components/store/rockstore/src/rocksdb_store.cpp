afe7dba60c82f525fda71058c6543742dc329d70
Daniel Waddington <daniel.waddington@ibm.com>
2018-05-11 03:27:29 -0700

    attempting to make rocksdb use less memory

--- src/components/store/rockstore/src/rocksdb_store.cpp
+++ src/components/store/rockstore/src/rocksdb_store.cpp
@@ -7,6 +7,7 @@
 #include <boost/filesystem.hpp>
 #include <tbb/concurrent_hash_map.h>
 #include "rocksdb/db.h"
+#include "rocksdb/table.h"
 
 #include "rocksdb_store.h"
 
@@ -29,7 +30,16 @@ IKVStore::pool_t RockStore::create_pool(const std::string path,
   rocksdb::Options options;
   options.error_if_exists = true;
   options.create_if_missing = true;
-  options.max_open_files = 1024;
+  options.max_open_files = MAX_OPEN_FILES;
+  options.optimize_filters_for_hits = true; /* if we get a lot of hits */
+  options.OptimizeForPointLookup(32);
+  options.use_direct_reads = true;
+  //options.allow_mmap_reads = true;
+  
+  rocksdb::BlockBasedTableOptions table_options;
+  table_options.block_cache = rocksdb::NewLRUCache(1024 * 1024 * 1024LL);
+  table_options.cache_index_and_filter_blocks = true;
+  options.table_factory.reset(NewBlockBasedTableFactory(table_options));
   
   std::string db_file = path + "/" + name;
   rocksdb::DB* db = nullptr;
@@ -45,14 +55,26 @@ IKVStore::pool_t RockStore::open_pool(const std::string path,
 {
   rocksdb::Options options;
   options.create_if_missing = true;
+  options.max_open_files = MAX_OPEN_FILES;
+  options.optimize_filters_for_hits = true; /* if we get a lot of hits */
+  options.OptimizeForPointLookup(32);
+  options.use_direct_reads = true;
+    
+  rocksdb::BlockBasedTableOptions table_options;
+  table_options.block_cache = rocksdb::NewLRUCache(1024 * 1024 * 1024LL);
+  table_options.cache_index_and_filter_blocks = true;
+  options.table_factory.reset(NewBlockBasedTableFactory(table_options));
 
+  
   std::string db_file = path + "/" + name;
   rocksdb::DB* db = nullptr;
   rocksdb::Status status;
-  if(flags & IKVStore::FLAGS_READ_ONLY)
+  if(flags & IKVStore::FLAGS_READ_ONLY) {
     status = rocksdb::DB::OpenForReadOnly(options, db_file, &db);
-  else
+  }
+  else {
     status = rocksdb::DB::Open(options, db_file, &db);
+  }
   
   if(!status.ok())
     throw General_exception("unable to open or create rocksDB database (%s)", db_file.c_str());
@@ -107,7 +129,10 @@ int RockStore::get(const pool_t pool,
 {
   rocksdb::DB * db = reinterpret_cast<rocksdb::DB*>(pool);
   std::string value;
-  rocksdb::Status status = db->Get(rocksdb::ReadOptions(), key, &value);
+  rocksdb::ReadOptions options;
+  options.fill_cache = false;
+
+  rocksdb::Status status = db->Get(options, key, &value);
   if(!status.ok()) {
     PLOG("RockStore::get - no key (%s)", key.c_str());
     return E_FAIL;
@@ -119,6 +144,13 @@ int RockStore::get(const pool_t pool,
   assert(out_value);
   memcpy(out_value, value.data(), out_value_len);
 
+
+  // std::string out;
+  // db->GetProperty("rocksdb.cur-size-all-mem-tables", &out);
+  // std::string out;
+  // db->GetProperty("rocksdb.estimate-table-readers-mem", &out);
+  // PLOG("### %s", out.c_str());
+ 
   return S_OK;
 }
 
