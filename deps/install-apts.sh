35ddb5db442c1e8fd9f13070d37299646a7517ad
Feng Li <fengggli@yahoo.com>
2018-05-16 22:15:59 -0700

    remove spaces and redundency

--- deps/install-apts.sh
+++ deps/install-apts.sh
@@ -4,11 +4,11 @@ apt-get install -y gcc libpciaccess-dev make libcunit1-dev pkg-config \
         cmake global gdb build-essential\
         sloccount doxygen synaptic libnuma-dev libaio-dev libcunit1 \
         libcunit1-dev libboost-system-dev libboost-iostreams-dev libboost-program-options-dev \
-        libboost-filesystem-dev \ 
+        libboost-filesystem-dev \
         libssl-dev g++-multilib fabric libtool-bin autoconf automake \
         rapidjson-dev libfuse-dev libpcap-dev sqlite3 libsqlite3-dev libomp-dev \
-	      libtbb-dev libtbb-doc libboost-python-dev libkmod-dev libjson-c-dev libbz2-dev \
-          linux-headers-`uname -r` libelf-dev 
+	libtbb-dev libtbb-doc libboost-python-dev libkmod-dev libjson-c-dev libbz2-dev \
+        linux-headers-`uname -r` libelf-dev 
 
 # optional
 #
