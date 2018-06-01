7819827e34587c0833ee3b5e835397446e1e72be
Daniel Waddington <daniel.waddington@ibm.com>
2018-06-01 10:02:23 -0700

    added multi-component and basic Put/Get tests

--- testing/kvstore/exp_put.h
+++ testing/kvstore/exp_put.h
@@ -1,3 +1,10 @@
+#ifndef __EXP_PUT_H__
+#define __EXP_PUT_H__
+
+#include "data.h"
+
+extern Data * _data;
+
 class Experiment_Put : public Core::Tasklet
 { 
 public:
@@ -6,7 +13,7 @@ public:
     assert(arg);
   }
   
-  void initialize(unsigned core) {
+  void initialize(unsigned core) override {
     char poolname[256];
     sprintf(poolname, "Put.pool.%u", core);
     PLOG("Creating pool for worker %u ...", core);
@@ -16,9 +23,14 @@ public:
     PLOG("Created pool for worker %u ...OK!", core);
     //    _pool = _store->open_pool("/dev/", "dax0.0");
     ProfilerRegisterThread();
+    _ready = true;
   };
-  
-  void do_work(unsigned core) {
+
+  bool ready() override {
+    return _ready;
+  }
+
+  void do_work(unsigned core) override {
     if(_first_iter) {
       _start = std::chrono::high_resolution_clock::now();
       _first_iter = false;
@@ -29,7 +41,7 @@ public:
     assert(rc == S_OK);
   }
   
-  void cleanup(unsigned core) {
+  void cleanup(unsigned core) override {
     _end = std::chrono::high_resolution_clock::now();
     double secs = std::chrono::duration_cast<std::chrono::milliseconds>(_end - _start).count() / 1000.0;
     PINF("*Put* (%u) IOPS: %2g", core, ((double) _i) / secs);
@@ -42,5 +54,9 @@ private:
   Component::IKVStore *                 _store;
   Component::IKVStore::pool_t           _pool;
   bool                                  _first_iter = true;
+  bool                                  _ready = false;
   std::chrono::system_clock::time_point _start, _end;
 };
+
+
+#endif //  __EXP_PUT_H__
