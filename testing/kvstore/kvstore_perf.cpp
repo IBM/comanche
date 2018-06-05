7f8f5662379f1ac6628dcc7b8f52639b462f4673
Feng Li <fengggli@yahoo.com>
2018-06-05 14:44:54 -0700

    nvmestore: test for nvmestore

--- testing/kvstore/kvstore_perf.cpp
+++ testing/kvstore/kvstore_perf.cpp
@@ -7,11 +7,14 @@
 #include <boost/program_options.hpp>
 #include <chrono>
 #include <gperftools/profiler.h>
+
+#include <core/dpdk.h>
+
 #define PATH "/mnt/pmem0/"
 //#define PATH "/dev/dax0.0"
 #define POOL_NAME "test.pool"
 
-#define PMSTORE_PATH "/home/danielwaddington/comanche/build/comanche-restricted/src/components/pmstore/libcomanche-pmstore.so"
+#define PMSTORE_PATH "libcomanche-pmstore.so"
 #define FILESTORE_PATH "libcomanche-storefile.so"
 #define NVMESTORE_PATH "libcomanche-nvmestore.so"
 #define ROCKSTORE_PATH "libcomanche-rocksdb.so"
@@ -28,6 +31,11 @@ Data * _data;
 static Component::IKVStore * g_store;
 static void initialize();
 static void cleanup();
+
+// required for nvmestore
+static Component::IBlock_device  *_block;
+static Component::IBlock_allocator *_alloc;
+static size_t _nr_blks; //number of blocks from IBlockdev
 
 struct {
   std::string test;
@@ -116,9 +124,77 @@ static void initialize()
   else if(Options.component == "filestore") {
     comp = Component::load_component(FILESTORE_PATH, Component::filestore_factory);
   }
-  // else if(Options.component == "nvmestore") {
-  //   comp = Component::load_component(NVMESTORE_PATH, Component::nvmestore_factory);
-  // }
+  else if(Options.component == "nvmestore") {
+
+    DPDK::eal_init(512);
+
+    /*
+     * init blk devices
+     */
+#ifdef USE_SPDK_NVME_DEVICE
+    
+    comp = Component::load_component("libcomanche-blknvme.so",
+                                                        Component::block_nvme_factory);
+
+    assert(comp);
+    PLOG("Block_device factory loaded OK.");
+    IBlock_device_factory * fact = (IBlock_device_factory *) comp->query_interface(IBlock_device_factory::iid());
+    
+    cpu_mask_t cpus;
+    cpus.add_core(2);
+
+    _block = fact->create("86:00.0", &cpus);
+
+    assert(_block);
+    fact->release_ref();
+    PINF("Lower block-layer component loaded OK.");
+
+#else
+    
+    comp = Component::load_component("libcomanche-blkposix.so",
+                                                        Component::block_posix_factory);
+    assert(comp);
+    PLOG("Block_device factory loaded OK.");
+
+    IBlock_device_factory * fact_blk = (IBlock_device_factory *) comp->query_interface(IBlock_device_factory::iid());
+    std::string config_string;
+    config_string = "{\"path\":\"";
+    //  config_string += "/dev/nvme0n1";1
+    config_string += "./blockfile.dat";
+    //  config_string += "\"}";
+    config_string += "\",\"size_in_blocks\":10000}";
+    PLOG("config: %s", config_string.c_str());
+
+    _nr_blks = 10000;
+
+    _block = fact_blk->create(config_string);
+    assert(_block);
+    fact_blk->release_ref();
+    PINF("Block-layer component loaded OK (itf=%p)", _block);
+#endif
+
+    /*
+     * instantiate block allocator
+     */
+    comp = load_component("libcomanche-blkalloc-aep.so",
+                                  Component::block_allocator_aep_factory);
+    assert(comp);
+    IBlock_allocator_factory * fact_blk_alloc = static_cast<IBlock_allocator_factory *>
+      (comp->query_interface(IBlock_allocator_factory::iid()));
+
+    size_t num_blocks = _nr_blks;
+    PLOG("Opening allocator to support %lu blocks", num_blocks);
+    _alloc = fact_blk_alloc->open_allocator(
+                                  num_blocks,
+                                  "nvmestore-blk-allc-1");  
+    fact_blk_alloc->release_ref();  
+
+    /*
+     * instantialize
+     */
+    comp = Component::load_component("libcomanche-nvmestore.so",
+                                                        Component::nvmestore_factory);
+  }
   else if(Options.component == "rockstore") {
     comp = Component::load_component(ROCKSTORE_PATH, Component::rocksdb_factory);
   }
@@ -127,13 +203,24 @@ static void initialize()
   assert(comp);
   IKVStore_factory * fact = (IKVStore_factory *) comp->query_interface(IKVStore_factory::iid());
 
-  g_store = fact->create("owner","name");  
+  if(Options.component == "nvmestore") {
+    g_store = fact->create("owner","name", _block, _alloc ); 
+  else{
+    g_store = fact->create("owner","name"); }
+  }
   fact->release_ref();
 }
 
 static void cleanup()
 {
   g_store->release_ref();
+
+  if(Options.component == "nvmestore") {
+    assert(_alloc);
+    assert(_block);
+    _alloc->release_ref();
+    _block->release_ref();
+  }
 }
 
 
