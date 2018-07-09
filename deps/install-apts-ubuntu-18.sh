a318497a2189ed22a10bee2b2c833cdbeb196ad5
Feng Li <fengggli@yahoo.com>
2018-07-09 14:57:20 -0700

    build: remove libtbb, use the one in submodule instead.

--- deps/install-apts-ubuntu-18.sh
+++ deps/install-apts-ubuntu-18.sh
@@ -7,6 +7,6 @@ apt-get install -y build-essential libnuma-dev libelf-dev libpcap-dev uuid-dev \
         libcunit1-dev libboost-system-dev libboost-iostreams-dev libboost-program-options-dev libboost-filesystem-dev \
         libssl-dev g++-multilib fabric libtool-bin autoconf automake libibverbs-dev librdmacm-dev \
         rapidjson-dev libfuse-dev libpcap-dev sqlite3 libsqlite3-dev libomp-dev \
-        libtbb-dev libtbb-doc libboost-python-dev libkmod-dev libjson-c-dev libbz2-dev \
+        libboost-python-dev libkmod-dev libjson-c-dev libbz2-dev \
         linux-headers-`uname -r` libelf-dev libsnappy-dev liblz4-dev \
         asciidoc xmlto libtool
