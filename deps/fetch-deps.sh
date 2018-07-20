ea556306fd622f74466d430695ee095dab6263a2
Clem Dickey <dickyecl@us.ibm.com>
2018-07-20 15:05:09 -0700

    fetch-deps: use subshells to remember base directory

--- deps/fetch-deps.sh
+++ deps/fetch-deps.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-BASE=$PWD
-
 function jumpto
 {
     label=$1
@@ -24,48 +22,58 @@ wget http://fast.dpdk.org/rel/dpdk-17.08.tar.xz
 #wget http://fast.dpdk.org/rel/dpdk-16.11.tar.xz
 tar -xf dpdk-17.08.tar.xz
 ln -s dpdk-17.08 dpdk
-cd dpdk
-mkdir -p usertools # weird hack
-cp ../dpdk-extras/build.sh  .
-cp ../dpdk-extras/defconfig_x86_64-native-linuxapp-gcc ./config
-cp ../dpdk-extras/dpdk-patches-17.08/eal_memory.c ./lib/librte_eal/linuxapp/eal/eal_memory.c
-./build.sh
-cd $BASE
+( cd dpdk
+  mkdir -p usertools # weird hack
+  cp ../dpdk-extras/build.sh  .
+  cp ../dpdk-extras/defconfig_x86_64-native-linuxapp-gcc ./config
+  cp ../dpdk-extras/dpdk-patches-17.08/eal_memory.c ./lib/librte_eal/linuxapp/eal/eal_memory.c
+  ./build.sh
+)
 
 # jumpto end # TEMPORARY!
 
 spdk:
 echo "Cloning SPDK v17.07.1 ..."
 git clone https://github.com/spdk/spdk.git
-#cd spdk && git checkout tags/v16.12
-cd spdk && git checkout fca11f1
-./configure --with-dpdk=../dpdk
-cp ../spdk-extras/build.sh .
-./build.sh
-cd $BASE
+( cd spdk
+  # git checkout tags/v16.12
+  git checkout fca11f1
+  ./configure --with-dpdk=../dpdk
+  cp ../spdk-extras/build.sh .
+  ./build.sh
+)
 
 city:
 echo "Cloning CityHash ..."
 git clone https://github.com/google/cityhash.git
-cd cityhash
-./configure && make
-make install
-cd $BASE
+( cd cityhash
+  ./configure && make
+  make install
+)
 
 echo "Cloning Google test framework (v1.8.0) ..."
 git clone https://github.com/google/googletest.git
-cd googletest
-git checkout tags/release-1.8.0
-cmake . && make
-make install
-cd $BASE
+( cd googletest
+  git checkout tags/release-1.8.0
+  cmake . && make
+  make install
+)
+
+echo "Cloning libfabric (v1.6.1) ..."
+git clone https://github.com/ofiwg/libfabric.git
+( cd libfabric
+  git checkout tags/v1.6.1
+  ./autogen.sh
+  ./configure && make
+  make install
+)
 
 # echo "Cloning Flatbuffers ..."
 # git clone https://github.com/google/flatbuffers.git
-# cd flatbuffers
-# cmake . && make
-# make install
-# cd $BASE
+# ( cd flatbuffers
+#   cmake . && make
+#   make install
+# )
 
 # END OF MANDATORY DEPS
 jumpto end
@@ -73,88 +81,88 @@ jumpto end
 nanomsg:
 echo "Fetching Nanomsg ..."
 git clone https://github.com/nanomsg/nanomsg.git
-cd nanomsg ; git checkout tags/1.0.0
-cd nanomsg ; ./configure && make ; make install
-cd $BASE
+( cd nanomsg ; git checkout tags/1.0.0
+  cd nanomsg ; ./configure && make ; make install
+)
 jumpto end
 
 protobuf:
 echo "Fetching Google protobuf..."
 wget https://github.com/google/protobuf/releases/download/v3.0.0/protobuf-cpp-3.0.0.tar.gz
 tar -xvf protobuf-cpp-3.0.0.tar.gz
-cd protobuf-3.0.0/ ; ./configure ; make ; make install
-cd $BASE
+( cd protobuf-3.0.0/ ; ./configure ; make ; make install
+)
 jumpto end
 
 messaging:
 echo "Cloning libsodium..."
 git clone git://github.com/jedisct1/libsodium.git
-cd libsodium
-./autogen.sh
-./configure && make check
-make install
-ldconfig
-cd $BASE
+( cd libsodium
+  ./autogen.sh
+  ./configure && make check
+  make install
+  ldconfig
+)
 jumpto end
 
 echo "Cloning libzmq..."
 git clone git://github.com/zeromq/libzmq.git
-cd libzmq
-./autogen.sh
-./configure && make check
-make install
-ldconfig
-cd $BASE
+( cd libzmq
+  ./autogen.sh
+  ./configure && make check
+  make install
+  ldconfig
+)
 jumpto end
 
 echo "Cloning CZMQ..."
 git clone git://github.com/zeromq/czmq.git
-cd czmq
-./autogen.sh
-./configure && make check
-make install
-ldconfig
-cd $BASE
+( cd czmq
+  ./autogen.sh
+  ./configure && make check
+  make install
+  ldconfig
+)
 jumpto end
 
 echo "Cloning libcurve..."
 git clone git://github.com/zeromq/libcurve.git
-cd libcurve
-sh autogen.sh
-./autogen.sh
-./configure && make check
-make install
-ldconfig
-cd $BASE
+( cd libcurve
+  sh autogen.sh
+  ./autogen.sh
+  ./configure && make check
+  make install
+  ldconfig
+)
 
 # glog:
 # echo "Google Glog (v0.3.5) ..."
 # git clone git clone https://github.com/google/glog.git
-# cd glog
-# git checkout tags/v0.3.5
-# automake --add-missing && ./configure
-# make
-# make install
-# cd $BASE
+# ( cd glog
+#   git checkout tags/v0.3.5
+#   automake --add-missing && ./configure
+#   make
+#   make install
+# )
 
 zyre:
 echo "Cloning Zyre..."
 git clone git://github.com/zeromq/zyre.git
-cd zyre
-./autogen.sh && ./configure && make check
-make install
-ldconfig
-cd $BASE
+( cd zyre
+  ./autogen.sh && ./configure && make check
+  make install
+  ldconfig
+)
 
 # moved to apt install
 #json:
 #echo "Cloning RapidJSON (v1.101)..."
 #git clone https://github.com/miloyip/rapidjson.git
-#cd rapidjson
-#git checkout tags/v1.1.0
-#cmake . && make
-#make install
-#cd $BASE
+#( rapidjson
+#  git checkout tags/v1.1.0
+#  cmake . && make
+#  make install
+#)
 
 # end of default install
 jumpto end
@@ -163,16 +171,13 @@ python:
 echo "Fetching Python 3.5 source..."
 wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tar.xz
 tar -xvf Python-3.5.2.tar.xz
-cd $BASE
 
 echo "Cloning NumPy source ..."
 git clone https://github.com/numpy/numpy.git
-cd $BASE
 
 echo "Fetching Armadillio C++ linear algebra library...."
 wget http://sourceforge.net/projects/arma/files/armadillo-7.500.0.tar.xz
 tar -xvf armadillo-7.500.0.tar.xz
-cd $BASE
 
 jumpto end
 
@@ -189,11 +194,11 @@ derecho:
 
 echo "Cloning derecho..."
 git clone --recursive https://github.com/Derecho-Project/derecho-unified.git
-cd derecho-unified
-mkdir build
-cd build
-cmake .. && make
-cd $BASE
+( cd derecho-unified
+  mkdir build
+  cd build
+  cmake .. && make
+)
 
 jumpto end
 
@@ -201,19 +206,20 @@ rocksdb:
 
 echo "Cloning RocksDB..."
 git clone https://github.com/facebook/rocksdb.git
-cd rocksdb && git checkout tags/v5.1.4
+( cd rocksdb && git checkout tags/v5.1.4
+)
 
 jumpto end
 
 pmem:
 git clone https://github.com/pmem/nvml.git
-cd nvml && git checkout tags/1.3
-cd $BASE
+( cd nvml && git checkout tags/1.3
+)
 
 echo "Cloning PMDK..."
 git clone https://github.com/dwaddington/pmdk.git
-cd pmdk && make && make install
-cd $BASE
+( cd pmdk && make && make install
+)
 
 jumpto end
 
