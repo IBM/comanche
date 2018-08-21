49037c869d454b52455cd6df16ff1b87f3d10488
Clem Dickey <dickyecl@us.ibm.com>
2018-08-21 14:49:50 -0700

    net/fabric: move buffer size choice to top-level of test

--- src/components/net/fabric/unit_test/registered_memory.h
+++ src/components/net/fabric/unit_test/registered_memory.h
@@ -15,14 +15,14 @@ class registered_memory
 {
   Component::IFabric_connection &_cnxn;
   /* There may be an alignment restriction on registered memory, May be 8, or 16. */
-  alignas(64) std::array<char, 4096> _memory;
+  std::vector<char> _memory;
   registration _registration;
 public:
   /*
    * NOTE: if the memory remote key is used (that is, if the mr attributes do not include FI_PROV_KEY),
    * the key must the unique among registered memories.
    */
-  explicit registered_memory(Component::IFabric_connection &cnxn, std::uint64_t remote_key);
+  explicit registered_memory(Component::IFabric_connection &cnxn, std::size_t size, std::uint64_t remote_key);
 
   char &operator[](std::size_t ix) { return _memory[ix]; }
   volatile char &first_char() { return _memory[0]; }
