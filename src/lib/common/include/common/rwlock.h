1df4713294ad6db75026cbb19e065780a12f1385
Daniel Waddington <daniel.waddington@ibm.com>
2018-09-19 05:39:05 -0700

    added lock guard and debug checks

--- src/lib/common/include/common/rwlock.h
+++ src/lib/common/include/common/rwlock.h
@@ -1,15 +1,19 @@
 #ifndef __COMMON_RWLOCK_H__
 #define __COMMON_RWLOCK_H__
 
+#define _MULTI_THREADED
+#include <common/exceptions.h>
 #include <pthread.h>
 
 namespace Common
 {
+
 class RWLock
 {
 public:
   RWLock() {
-    pthread_rwlock_init(&_lock, NULL); 
+    if(pthread_rwlock_init(&_lock, NULL))
+      throw General_exception("unable to initialize RW lock");
   }
 
   int read_lock() {
@@ -38,6 +42,38 @@ public:
   
 private:
   pthread_rwlock_t _lock;
+};
+
+
+class RWLock_guard
+{
+public:
+  enum {
+    WRITE = 0,
+    READ = 1,
+  };
+
+public:
+  RWLock_guard(RWLock& lock, int mode = READ) : _lock(lock) {
+    int rc;
+    if(mode == WRITE) {
+      if(_lock.write_lock() != 0)
+        throw General_exception("failed to take write lock");
+    }
+    else if(mode == READ) {
+      if(_lock.read_lock() != 0)
+        throw General_exception("failed to take read lock");
+    }
+    else throw API_exception("unexpected RWLock_guard mode (mode=%d)", mode);
+  }
+  
+  ~RWLock_guard() {
+    if(_lock.unlock() != 0)
+      throw General_exception("failed to release lock in guard");
+  }
+
+private:
+  RWLock& _lock;
 };
 
 }
