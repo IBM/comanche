987f50e92f00ff8668931569df373c6def9eeeba
Daniel Waddington <daniel.waddington@ibm.com>
2018-11-09 04:44:54 -0800

    new options + fixes

--- testing/kvstore/kvstore_perf.cpp
+++ testing/kvstore/kvstore_perf.cpp
@@ -37,18 +37,21 @@ static void cleanup();
 
 int g_argc;
 char ** g_argv;
+
 pthread_mutex_t g_write_lock = PTHREAD_MUTEX_INITIALIZER;
+boost::program_options::options_description desc("Options"); 
 
 int main(int argc, char * argv[])
 {
   ProfilerDisable();
   
-    g_argc = argc;
-    g_argv = argv;
+  g_argc = argc;
+  g_argv = argv;
 
   namespace po = boost::program_options; 
-  po::options_description desc("Options"); 
-  desc.add_options()
+
+  try {
+    desc.add_options()
     ("help", "Show help")
     ("test", po::value<std::string>(), "Test name <all|put|get|put_direct|get_direct>")
     ("component", po::value<std::string>(), "Implementation selection <pmstore|nvmestore|filestore|mapstore|hstore>")
@@ -67,11 +70,11 @@ int main(int argc, char * argv[])
     ("owner", po::value<std::string>(), "Owner name for component registration")
     ("server_address", po::value<std::string>(), "server address, with port")
     ("device_name", po::value<std::string>(), "device name")
-    ("verbose", po::value<bool>(), "verbose output (boolean)")
+    ("pci_addr", po::value<std::string>(), "PCI address (e.g. 0b:00.0)")
+    ("verbose", po::value<bool>(), "verbose output (boolean)")    
     ("summary", po::value<bool>(), "prints summary statement: most frequent latency bin info per core")
     ;
 
-  try {
     po::variables_map vm; 
     po::store(po::parse_command_line(argc, argv, desc),  vm);
 
@@ -97,26 +100,41 @@ int main(int argc, char * argv[])
     Options.server_address = vm.count("server_address") ? vm["server_address"].as<std::string>() : "127.0.0.1";
     Options.device_name = vm.count("device_name") ? vm["device_name"].as<std::string>() : "unused";
 
-/*
-    if(vm.count("path"))
-    {
-        Options.path = vm["path"].as<std::string>();
+    if(Options.component == "nvmestore") {
+      if(!vm.count("pci_addr")) {
+        std::cout << "Must specify --pci_addr option for nvmestore component\n";
+        return -1;
+      }
+      Options.pci_addr = vm["pci_addr"].as<std::string>();
     }
-    else 
-    {
-        Options.path = DEFAULT_PATH;
+    else if(Options.component == "pmstore" || Options.component == "htstore") {
+      if(vm.count("path") == 0) {
+        std::cout << "Must specify --path option for persistent memory store\n";
+        return -1;
+      }
     }
-*/
+    
+    /*
+      if(vm.count("path"))
+      {
+      Options.path = vm["path"].as<std::string>();
+      }
+      else 
+      {
+      Options.path = DEFAULT_PATH;
+      }
+    */
     Options.elements = vm.count("elements") > 0 ? vm["elements"].as<unsigned int>() : 100000;
     Options.key_length = vm.count("key_length") > 0 ? vm["key_length"].as<unsigned int>() : 8;
     Options.value_length = vm.count("value_length") > 0 ? vm["value_length"].as<unsigned int>() : 64; 
   }
   catch (const po::error &ex)
-  {
-    std::cerr << ex.what() << '\n';
-  }
+    {
+      std::cerr << "option parsing failed unexpectedly\n";
+      std::cerr << ex.what() << '\n';
+    }
 
-  bool use_direct_memory = Options.component.compare("dawn_client") == 0;
+  bool use_direct_memory = Options.component == "dawn_client";
   _data = new Data(Options.elements, Options.key_length, Options.value_length, use_direct_memory);
   initialize();
 
@@ -130,29 +148,25 @@ int main(int argc, char * argv[])
 
   ProfilerStart("cpu.profile");
 
-  if (Options.test == "all" || Options.test == "put")
-  {
-      Core::Per_core_tasking<ExperimentPut, ProgramOptions> exp(cpus, Options);
-      exp.wait_for_all();
+  if (Options.test == "all" || Options.test == "put") {
+    Core::Per_core_tasking<ExperimentPut, ProgramOptions> exp(cpus, Options);
+    exp.wait_for_all();
   }
 
-  if (Options.test == "all" || Options.test == "get")
-  {
-      Core::Per_core_tasking<ExperimentGet, ProgramOptions> exp(cpus, Options);
+  if (Options.test == "all" || Options.test == "get") {
+    Core::Per_core_tasking<ExperimentGet, ProgramOptions> exp(cpus, Options);
 
-      exp.wait_for_all();
+    exp.wait_for_all();
   }
 
-  if (Options.test == "all" || Options.test == "get_direct")
-  {
-      Core::Per_core_tasking<ExperimentGetDirect, ProgramOptions> exp(cpus, Options);
-      exp.wait_for_all();
+  if (Options.test == "all" || Options.test == "get_direct") {
+    Core::Per_core_tasking<ExperimentGetDirect, ProgramOptions> exp(cpus, Options);
+    exp.wait_for_all();
   }
 
-  if (Options.test == "all" || Options.test == "put_direct")
-  {
-      Core::Per_core_tasking<ExperimentPutDirect, ProgramOptions> exp(cpus, Options);
-      exp.wait_for_all();
+  if (Options.test == "all" || Options.test == "put_direct") {
+    Core::Per_core_tasking<ExperimentPutDirect, ProgramOptions> exp(cpus, Options);
+    exp.wait_for_all();
   } 
   ProfilerStop();
   
@@ -178,18 +192,15 @@ static void initialize()
   else if(Options.component == "rockstore") {
     comp = Component::load_component(ROCKSTORE_PATH, Component::rocksdb_factory);
   }
-  else if(Options.component == "dawn_client")
-  {
+  else if(Options.component == "dawn_client") {
     
-      DECLARE_STATIC_COMPONENT_UUID(dawn_client_factory, 0xfac66078,0xcb8a,0x4724,0xa454,0xd1,0xd8,0x8d,0xe2,0xdb,0x87);  // TODO: find a better way to register arbitrary components to promote modular use
+    DECLARE_STATIC_COMPONENT_UUID(dawn_client_factory, 0xfac66078,0xcb8a,0x4724,0xa454,0xd1,0xd8,0x8d,0xe2,0xdb,0x87);  // TODO: find a better way to register arbitrary components to promote modular use
     comp = Component::load_component(DAWN_PATH, dawn_client_factory);
   }
-  else if (Options.component == "hstore")
-  {
+  else if (Options.component == "hstore") {
     comp = Component::load_component("libcomanche-hstore.so", Component::hstore_factory);
   }
-  else if (Options.component == "mapstore")
-  {
+  else if (Options.component == "mapstore") {
     comp = Component::load_component("libcomanche-storemap.so", Component::mapstore_factory);
   }
   else throw General_exception("unknown --component option (%s)", Options.component.c_str());
@@ -197,15 +208,14 @@ static void initialize()
   assert(comp);
   IKVStore_factory * fact = (IKVStore_factory *) comp->query_interface(IKVStore_factory::iid());
 
-  if(Options.component == "nvmestore"){
-    g_store = fact->create("owner","name", "09:00.0");
+  if(Options.component == "nvmestore") {
+    g_store = fact->create("owner","name", Options.pci_addr);
   }
-  else if (Options.component == "dawn_client")
-  {
-      g_store = fact->create(Options.debug_level, Options.owner, Options.server_address, Options.device_name);
+  else if (Options.component == "dawn_client") {
+    g_store = fact->create(Options.debug_level, Options.owner, Options.server_address, Options.device_name);
   }
-  else{
-    g_store = fact->create("owner",Options.owner);
+  else {
+    g_store = fact->create("owner", Options.owner);
   }
   fact->release_ref();
 }
