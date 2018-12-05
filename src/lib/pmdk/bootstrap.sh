448cbd02833eebc0bab3aeabccdd7a0ed723d778
Daniel Waddington <daniel.waddington@ibm.com>
2018-12-05 10:01:00 -0800

    pmdk and ndctl now clone into binary tree

--- src/lib/pmdk/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-echo "Boot-strapping PMDK (pmem.io) ..."
-
-if [ ! -d ./pmdk ] ; then
-    git clone https://github.com/dwaddington/pmdk.git
-#    cd pmdk ; git checkout tags/1.4 
-fi
-
