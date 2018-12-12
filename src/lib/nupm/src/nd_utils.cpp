58d244b1fd62cf17a8803ff219f1544c5bc6e61c
Daniel Waddington <danielwaddington@ibm.com>
2018-12-12 11:25:50 -0800

    additional debugging

--- src/lib/nupm/src/nd_utils.cpp
+++ src/lib/nupm/src/nd_utils.cpp
@@ -74,7 +74,8 @@ ND_control::ND_control() : _n_sockets(numa_num_configured_nodes())
     if(strcmp(ndctl_region_get_type_name(region),"pmem") == 0) {
 
       if(option_DEBUG)
-        PLOG("region:%d type:%s interleaves:%d numa-node:%d dev:%s size:%llu",         
+        PLOG("region:%d (%p) type:%s interleaves:%d numa-node:%d dev:%s size:%llu",
+             (void*) ndctl_region_get_resource(region),
              ndctl_region_get_id(region),
              ndctl_region_get_type_name(region),
              ndctl_region_get_interleave_ways(region),
