2f05da05196e2302cca4987d841a3855ebd7bfaf
Daniel Waddington <daniel.waddington@ibm.com>
2019-01-23 15:55:56 -0800

    still looking for timing mismatch

--- testing/kvstore/exp_throughput.h
+++ testing/kvstore/exp_throughput.h
@@ -9,6 +9,7 @@
 
 #include "experiment.h"
 #include "statistics.h"
+#include "stopwatch.h"
 
 extern Data * _data;
 extern pthread_mutex_t g_write_lock;
@@ -19,6 +20,7 @@ public:
   std::chrono::high_resolution_clock::time_point _start_time, _end_time;
   static unsigned long _iops;
   static std::mutex _iops_lock;
+  Stopwatch _sw;
   
   ExperimentThroughput(struct ProgramOptions options): Experiment(options) 
   {
@@ -37,14 +39,13 @@ public:
       {
         PLOG("[%u] Starting Throughput experiment (value len:%lu)...", core, g_data->value_len());
         _first_iter = false;
-#if 1
-	/* DAX inifialization is serialized by thread (due to libpmempool behavior).
-	 * It is in some sense unfair to start measurement in initialize_custom,
-	 * which occurs before DAX initialization. Reset start of measurement to first
-	 * put operation.
-	 */
+        
+        /* DAX inifialization is serialized by thread (due to libpmempool behavior).
+         * It is in some sense unfair to start measurement in initialize_custom,
+         * which occurs before DAX initialization. Reset start of measurement to first
+         * put operation.
+         */
         _start_time = std::chrono::high_resolution_clock::now();
-#endif
       }     
 
     // end experiment if we've reached the total number of components
@@ -54,9 +55,12 @@ public:
         return false;
       }
 
-    assert(g_data);
+    // assert(g_data);
+    _sw.start();
     int rc = _store->put(_pool, g_data->key_as_string(_i), g_data->value(_i), g_data->value_len());
     assert(rc == S_OK);
+    _sw.stop();
+    
 
     _i++;
     return true;
@@ -65,8 +69,12 @@ public:
   void cleanup_custom(unsigned core)  
   {
     _end_time = std::chrono::high_resolution_clock::now();
+    _sw.stop();
 
+    PLOG("stopwatch : %g secs", _sw.get_time_in_seconds());
     double secs = std::chrono::duration_cast<std::chrono::milliseconds>(_end_time - _start_time).count() / 1000.0;
+    PLOG("wall clock: %g secs", secs);
+    
     unsigned long iops = (unsigned long) ((double) _pool_num_objects) / secs;
     PLOG("%lu iops (core=%u)", iops, core);
     _iops_lock.lock();
