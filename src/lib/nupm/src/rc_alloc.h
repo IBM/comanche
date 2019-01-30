87a28105f7e8711b56d8d62226a12cbc39c40137
Daniel Waddington <danielwaddington@ibm.com>
2019-01-29 17:10:30 -0800

    initial rp_alloc using memkind

--- src/lib/nupm/src/rc_alloc.h
+++ src/lib/nupm/src/rc_alloc.h
@@ -1,12 +1,41 @@
 #ifndef __NUPM_RC_ALLOC__
 #define __NUPM_RC_ALLOC__
 
-
+#include <string>
 #include <common/memory.h>
-
 
 namespace nupm
 {
+class Rca_AVL_internal;
+
+/** 
+ * Reconstituting allocator
+ * 
+ * 
+ */
+class Rca_AVL : public Common::Reconstituting_allocator
+{
+ public:
+  Rca_AVL();
+  ~Rca_AVL();
+
+  void add_managed_region(void * region_base, size_t region_length, int numa_node);
+  void add_managed_region(const std::string& pmem_file, int numa_node);
   
+  void inject_allocation(void * ptrm, size_t size, int numa_node) override;
+
+  void *alloc(size_t size,
+              int numa_node,
+              size_t alignment = 0) override;
+
+  void free(void *ptr, int numa_node) override;
+  
+ private:
+  Rca_AVL_internal * _rca;
+
+};
+
+
+
 }
 #endif
