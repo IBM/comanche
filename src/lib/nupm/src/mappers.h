471f2dedd235cad877a7775bf07d29df7460d99a
Daniel Waddington <daniel.waddington@ibm.com>
2019-02-08 16:37:16 -0800

    new Rc_alloc_LB mapper

--- src/lib/nupm/src/mappers.h
+++ src/lib/nupm/src/mappers.h
@@ -4,6 +4,62 @@
 namespace nupm
 {
 
+class Single_threshold_bucket_mapper
+{
+private:
+
+  const size_t _other_bucket_index;
+  static constexpr size_t L0_MAX_SMALL_OBJECT_SIZE = KiB(4);
+  static constexpr size_t L0_REGION_SIZE = MiB(2);
+  
+public:
+  Single_threshold_bucket_mapper() :
+    _other_bucket_index(get_log2_bin(KB(4))) {
+  }
+  
+  unsigned bucket(size_t object_size) {    
+    if(object_size <= L0_MAX_SMALL_OBJECT_SIZE)
+      return get_log2_bin(object_size);
+    else
+      return _other_bucket_index;
+  }
+
+  void * base(void * addr, size_t object_size) {
+    if(object_size <= L0_MAX_SMALL_OBJECT_SIZE )
+      return round_down(addr, KB(4));
+    else
+      return addr;
+  }
+
+  size_t region_size(size_t object_size) {
+    if(object_size <= L0_MAX_SMALL_OBJECT_SIZE)
+      return L0_REGION_SIZE;
+    else
+      return object_size;
+  }
+
+  size_t rounded_up_object_size(size_t object_size) {
+    if(object_size <= L0_MAX_SMALL_OBJECT_SIZE)
+      return (1UL << get_log2_bin(object_size));
+    else
+      return object_size;
+  }
+  
+private:
+  inline static unsigned get_log2_bin(size_t a) {
+    unsigned fsmsb = unsigned(((sizeof(size_t) * 8) - __builtin_clzl(a)));
+    if ((addr_t(1) << (fsmsb - 1)) == a) fsmsb--;
+    return fsmsb;
+  }
+
+  inline static void *round_down(void *p, unsigned long alignment) {
+    if (mword_t(p) % alignment == 0) return p;
+    return reinterpret_cast<void *>(mword_t(p) & ~(mword_t(alignment) - 1UL));
+  }
+
+  
+};
+  
 class Log2_bucket_mapper
 {
   static constexpr size_t REGION_SIZE = GB(1);
@@ -42,7 +98,8 @@ private:
 };
 
 
-using Bucket_mapper = Log2_bucket_mapper;
+//using Bucket_mapper = Log2_bucket_mapper;
+using Bucket_mapper = Single_threshold_bucket_mapper;
   
 
   
