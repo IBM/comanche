d6bd79b309c2075c696191c1e21fa93c13edf038
Luna Xu <xuluna@ibm.com>
2019-02-12 09:51:22 -0800

    formatted

--- src/components/index/ramrbtree/unit_test/test1.cpp
+++ src/components/index/ramrbtree/unit_test/test1.cpp
@@ -1,87 +1,91 @@
 /* note: we do not include component source, only the API definition */
-#include <gtest/gtest.h>
-#include <common/utils.h>
 #include <api/components.h>
 #include <api/kvindex_itf.h>
+#include <common/str_utils.h>
+#include <common/utils.h>
+#include <gtest/gtest.h>
+#include <ctime>
 
 #define COUNT 1000000
+#define LENGTH 16
 
 using namespace Component;
 using namespace Common;
 using namespace std;
 
-namespace {
-
+namespace
+{
 // The fixture for testing class Foo.
 class KVIndex_test : public ::testing::Test {
-
  protected:
-
   // If the constructor and destructor are not enough for setting up
   // and cleaning up each test, you can define the following methods:
-  
-  virtual void SetUp() {
+
+  virtual void SetUp()
+  {
     // Code here will be called immediately after the constructor (right
     // before each test).
   }
 
-  virtual void TearDown() {
+  virtual void TearDown()
+  {
     // Code here will be called immediately after each test (right
     // before the destructor).
   }
-  
+
   // Objects declared here can be used by all tests in the test case
-  static Component::IKVIndex * _kvindex;
+  static Component::IKVIndex *_kvindex;
 };
 
-Component::IKVIndex * KVIndex_test::_kvindex;
-
+Component::IKVIndex *KVIndex_test::_kvindex;
 
 TEST_F(KVIndex_test, Instantiate)
 {
   /* create object instance through factory */
-  Component::IBase * comp = Component::load_component("libcomanche-storeindex.so", Component::ramrbtree_factory);
+  Component::IBase *comp = Component::load_component(
+      "libcomanche-storeindex.so", Component::ramrbtree_factory);
 
   ASSERT_TRUE(comp);
-  IKVIndex_factory * fact = (IKVIndex_factory *) comp->query_interface(IKVIndex_factory::iid());
+  IKVIndex_factory *fact =
+      (IKVIndex_factory *) comp->query_interface(IKVIndex_factory::iid());
 
-  _kvindex = fact->create("owner","name");
-  
+  _kvindex = fact->create("owner", "name");
+
   fact->release_ref();
 }
 
 TEST_F(KVIndex_test, Insert)
 {
-    string keys[COUNT];
-    for(int i=0; i<COUNT; i++){
-        _kvindex->insert(key);
-    }
-  PINF("Size: %d", _kvindex->count());
+  string *keys = new string[COUNT];
+  for (int i = 0; i < COUNT; i++) {
+    keys[i] = random_string(LENGTH);
+  }
+  clock_t start = clock();
+  for (int i = 0; i < COUNT; i++) {
+    _kvindex->insert(keys[i]);
+  }
+  delete[] keys;
+  double duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+  PINF("Time sec: %lf", duration);
+  PINF("Size: %ld", _kvindex->count());
 }
 
 TEST_F(KVIndex_test, Get)
 {
-  std::string a=_kvindex->get(0);
+  std::string a = _kvindex->get(0);
   PINF("Key= %s", a.c_str());
-  a=_kvindex->get(1);
+  a = _kvindex->get(1);
   PINF("Key= %s", a.c_str());
 }
 
+TEST_F(KVIndex_test, Erase) { _kvindex->erase("MyKey"); }
 
-TEST_F(KVIndex_test, Erase)
+TEST_F(KVIndex_test, Count) { PINF("Size: %d", _kvindex->count()); }
+
+}  // namespace
+
+int main(int argc, char **argv)
 {
-  _kvindex->erase("MyKey");
-}
-
-TEST_F(KVIndex_test, Count){
-    PINF("Size: %d", _kvindex->count());
-}
-
-
-} // namespace
-
-int main(int argc, char **argv) {
-  
   ::testing::InitGoogleTest(&argc, argv);
   auto r = RUN_ALL_TESTS();
 
--- src/components/index/ramrbtree/unit_test/test2.cpp
+++ src/components/index/ramrbtree/unit_test/test2.cpp
@@ -5,15 +5,15 @@
 
 using namespace std;
 
-int main(){
-    RamRBTree tree;
+int main()
+{
+  RamRBTree tree;
 
-    tree.insert("abc");
-    tree.insert("efg");
-    cout<<tree.get(0)<<endl;
-    cout<<tree.get(1)<<endl;
-    cout<<tree.count()<<endl;
-    tree.get(3);
-    return 0;
+  tree.insert("abc");
+  tree.insert("efg");
+  cout << tree.get(0) << endl;
+  cout << tree.get(1) << endl;
+  cout << tree.count() << endl;
+  tree.get(3);
+  return 0;
 }
-
