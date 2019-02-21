b9fe2158b3ae49ab2a38424e4742c39e36f86a3f
Daniel Waddington <daniel.waddington@ibm.com>
2019-02-21 15:56:03 -0800

    devdax configuration howto

---
+++ doc/HOWTO.emulate.pm
@@ -0,0 +1,10 @@
+To configure "simulated" persistent memory to the kernel, add (modified) boot option:
+
+    memmap=X!Y where X is rsize, and Y is offset to allocate from.  For example:
+
+    memmap=32G!480G
+
+
+By default it will be fsdax.  To provision as devdax:
+
+    sudo ndctl create-namespace --mode devdax --map mem -e namespace0.0 -f
