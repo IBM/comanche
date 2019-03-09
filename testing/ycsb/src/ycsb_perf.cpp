cf28ef21224e4b7f63ceb840bb2a5d3e1eb5006c
Luna Xu <xuluna@ibm.com>
2019-03-08 16:11:46 -0800

    add multithreading

--- testing/ycsb/src/ycsb_perf.cpp
+++ testing/ycsb/src/ycsb_perf.cpp
@@ -1,8 +1,9 @@
 #include "ycsb_perf.h"
 #include <stdlib.h>
 #include <iostream>
+#include "../../kvstore/experiment.h"
 #include "../../kvstore/stopwatch.h"
-#include "db.h"
+#include "args.h"
 #include "db_fact.h"
 #include "workload.h"
 
@@ -13,30 +14,44 @@ int main(int argc, char * argv[])
     if(argc<4)
         show_program_options();
 
-    string operation=argv[1];
+    string operation = argv[1];
     string filename=argv[3];
 
-  ifstream input(filename);
+    ifstream input(filename);
 
-  try {
-    props.load(input);
-  }
-  catch (const string &msg) {
-    cerr << msg << endl;
+    try {
+      props.load(input);
+    }
+    catch (const string &msg) {
+      cerr << msg << endl;
+      input.close();
+      return -1;
+    }
+
     input.close();
-    return -1;
-  }
 
-  input.close();
-  ycsb::DB *db = ycsb::DBFactory::create(props);
-  assert(db);
-  ycsb::Workload* wl = new ycsb::Workload(props, db);
+    cpu_mask_t cpus;
 
-  // cpu_mask_t cpus;
+    try {
+      cpus =
+          Experiment::get_cpu_mask_from_string(props.getProperty("cores", "0"));
+    }
+    catch (...) {
+      PERR("%s", "couldn't create CPU mask. Exiting.");
+      return 1;
+    }
 
-  wl->load();
-  if (operation == "run") wl->run();
-  return 0;
+    if (operation == "run") props.setProperty("run", "1");
+
+    ycsb::DB *db = ycsb::DBFactory::create(props);
+    assert(db);
+    Args                                   args(db, props);
+    Core::Per_core_tasking<ycsb::Workload, Args> exp(cpus, args);
+    exp.wait_for_all();
+    auto first_exp = exp.tasklet(cpus.first_core());
+    first_exp->summarize();
+
+    return 0;
 }
 
 
