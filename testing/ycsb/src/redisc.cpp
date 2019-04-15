6b0ceb33e4ab5bbbbbdf2e1678036de567e8fbe3
Luna Xu <xuluna@ibm.com>
2019-04-15 10:19:58 -0700

    print port

--- testing/ycsb/src/redisc.cpp
+++ testing/ycsb/src/redisc.cpp
@@ -19,7 +19,12 @@ void RedisC::init(Properties &props, unsigned core)
   int    rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   port += rank / 6;
-  c              = redisConnect(host.c_str(), port);
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int  name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
+  cout << "host: " << processor_name << ", address: " << host
+       << ", port: " << port << endl;
+  c = redisConnect(host.c_str(), port);
   assert(c);
   if (c->err) {
     cout << c->errstr << endl;
