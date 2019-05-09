12299d6a01186be90e4002b99b2db878ecefdeef
Daniel Waddington <daniel.waddington@ibm.com>
2019-05-09 16:07:38 -0700

    placeholder

---
+++ src/kernel/modules/mcas/mcas.h
@@ -0,0 +1,61 @@
+#define MCAS_MINOR 100
+
+typedef uint64_t addr_t;
+
+enum {
+  IOCTL_CMD_EXPOSE = 9,
+};
+
+typedef struct
+{
+   void* vaddr;
+   size_t 
+}
+ __attribute__((packed)) IOCTL_EXPOSE_in;
+
+// enum {
+//   IOCTL_CMD_GETBITMAP = 9,
+//   IOCTL_CMD_GETPHYS = 10,
+//   IOCTL_CMD_SETMEMORY = 11,
+// };
+
+// typedef enum {
+//   MEMORY_UC = 1,
+//   MEMORY_WB = 2,
+//   MEMORY_WT = 3,
+// } memory_t;
+
+// typedef struct 
+// {
+//   void * ptr;
+//   size_t size;
+//   uint32_t    flags;
+//   void * out_data;
+//   size_t out_size;
+// }
+// __attribute__((packed)) IOCTL_GETBITMAP_param;
+
+// typedef struct {
+//   uint32_t magic;
+//   uint32_t bitmap_size;
+//   char     bitmap[0];
+// } __attribute__((packed)) IOCTL_GETBITMAP_out_param;
+
+// typedef struct
+// {
+//   addr_t vaddr;
+//   addr_t out_paddr;
+// }
+// __attribute__((packed)) IOCTL_GETPHYS_param;
+
+
+// typedef struct
+// {
+//   void*    vaddr;
+//   memory_t type;
+//   size_t   size;
+// }
+// __attribute__((packed)) IOCTL_SETMEMORY_param;
+
+
+
