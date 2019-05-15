49809ab33ee517246734ecf10787199e443dcb3c
Daniel Waddington <daniel.waddington@ibm.com>
2019-05-15 16:33:54 -0700

    wip

--- src/kernel/modules/mcas/testcli.cc
+++ src/kernel/modules/mcas/testcli.cc
@@ -16,6 +16,8 @@
 #include <fcntl.h>
 #include <vector>
 
+#include <nupm/nupm.h>
+
 #define REDUCE_KB(X) (X >> 10)
 #define REDUCE_MB(X) (X >> 20)
 #define REDUCE_GB(X) (X >> 30)
@@ -26,14 +28,35 @@
 #define GB(X) (((unsigned long) X) << 30)
 #define TB(X) (((unsigned long) X) << 40)
 
+#ifndef MAP_HUGETLB
+#define MAP_HUGETLB 0x40000 /* arch specific */
+#endif
+
+#ifndef MAP_HUGE_SHIFT
+#define MAP_HUGE_SHIFT 26
+#endif
+
+#ifndef MAP_HUGE_MASK
+#define MAP_HUGE_MASK 0x3f
+#endif
 
 int main()
 {
-  
+  //  Devdax_manager ddm({{"/dev/dax0.3", 0x9000000000, 0},
+  size_t size = 8000000;
   int fd = open("/dev/mcas", O_RDWR);
   assert(fd != -1);
   
-  void * ptr = aligned_alloc(KB(4), MB(4));
+  //  void * ptr = aligned_alloc(KB(4), MB(4));
+  void * ptr;
+  int flags = (MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_FIXED);
+  //  flags |= (shift & MAP_HUGE_MASK) << MAP_HUGE_SHIFT;
+  ptr = mmap(((void*) 0x910000000),
+             size,
+             PROT_READ|PROT_WRITE,
+             flags,
+             -1,
+             0);
 
   assert(ptr);
   memset(ptr, 0, MB(4));
@@ -41,9 +64,9 @@ int main()
   
 
   IOCTL_EXPOSE_msg ioparam;
-  ioparam.auth_token == 666;
+  ioparam.token = 666;
   ioparam.vaddr = ptr;
-  ioparam.vaddr_size = KB(4);
+  ioparam.vaddr_size = size;
 
   int rc = ioctl(fd, IOCTL_CMD_EXPOSE, &ioparam);  //ioctl call
   if(rc != 0) {
@@ -52,5 +75,6 @@ int main()
     return 0;
   }
 
+  munmap(ptr, size);
   close(fd);
 }
