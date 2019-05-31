4df4c56ecd74791ca6ea380d1c6fcf8bacd32a8d
Daniel Waddington <daniel.waddington@ibm.com>
2019-05-31 14:06:59 -0700

    mcas module userlevel API; to be tested

--- src/lib/nupm/src/mcas_mod.cpp
+++ src/lib/nupm/src/mcas_mod.cpp
@@ -8,9 +8,12 @@
 #include <fcntl.h>
 
 /* todo: unify with module header file */
+
+//------------------------------------------------------------
 enum {
   IOCTL_CMD_EXPOSE = 8,
   IOCTL_CMD_REMOVE = 9,
+  IOCTL_CMD_QUERY  = 10,
 };
 
 typedef struct
@@ -26,6 +29,16 @@ typedef struct
   uint64_t token; /* token of previously exposed memory */
 }
 __attribute__((packed)) IOCTL_REMOVE_msg;  
+
+typedef struct
+{
+  union {
+    uint64_t token; /* token of previously exposed memory */
+    size_t size;
+  };
+}
+__attribute__((packed)) IOCTL_QUERY_msg;  
+//------------------------------------------------------------
 
 
 
@@ -62,8 +75,8 @@ status_t nupm::revoke_memory(Memory_token token)
 }
 
 void * nupm::mmap_exposed_memory(Memory_token token,
-                                size_t& size,
-                                void* target_addr)
+                                 size_t& size,
+                                 void* target_addr)
 {
   int fd = open("/dev/mcas", O_RDWR, 0666);
   if(fd == -1) {
@@ -71,6 +84,17 @@ void * nupm::mmap_exposed_memory(Memory_token token,
     return nullptr;
   }
 
+  /* use ioctl to get size of area before calling mmap */
+  IOCTL_QUERY_msg msg = {token};
+  status_t rc = ioctl(fd, IOCTL_CMD_QUERY, &msg);
+
+  if(rc) {
+    PERR("nupm::map ioctl failed");
+    return nullptr;
+  }
+  
+  size = msg.size;
+  
   offset_t offset = ((offset_t)token) << 12; /* must be 4KB aligned */
 
   void * ptr = ::mmap(target_addr,
