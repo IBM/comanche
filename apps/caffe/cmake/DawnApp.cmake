d7d68a5cb34642136d6d0de851873658f960272d
Yanzhao Wu <Yanzhao.Wu@ibm.com>
2019-07-26 11:23:41 -0700

    Add Caffe Modification

---
+++ apps/caffe/cmake/DawnApp.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_CXX_STANDARD 11)
+
+set(ENV{DAWNAPP_HOME} "$ENV{HOME}/git/dawn-app")
+
+include_directories($ENV{DAWNAPP_HOME}/include)
+
---
+++ apps/caffe/gpu_direct_wrapper/setup_gdrcopy.sh
@@ -0,0 +1,5 @@
+GDRCOPY_HOME=$HOME/git/comanche/deps/gdrcopy
+cd $GDRCOPY_HOME
+./insmod.sh
+./validate
+
