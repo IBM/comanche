861de2ccce656095ce6b7e6c23980c8561b2dbd4
Nara-Batsoyol <Nara.Batsoyol@ibm.com>
2023-08-30 23:37:17 +0000

    Convert timestamp to arrow timestamp

--- dpu_filter/arrowfile/create_parquet.py
+++ dpu_filter/arrowfile/create_parquet.py
@@ -2,20 +2,24 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 
-# Sample data
+# Generate sample data with 6000 rows
+num_rows = 200
 data = {
-    'timestamp': pd.date_range(start='2023-08-01', periods=10, freq='D'),
-    'ID': [101, 102, 103, 104, 105, 106, 107, 108, 109, 110]
+    'timestamp': pd.date_range(start='2023-08-01', periods=num_rows, freq='D'),
+    'ID': range(101, 101 + num_rows)
 }
 
 # Create a Pandas DataFrame
 df = pd.DataFrame(data)
 
-# Convert to PyArrow Table
-table = pa.Table.from_pandas(df)
+# Convert timestamp to Arrow timestamp data type
+arrow_timestamps = pa.array(df['timestamp'], type=pa.timestamp('s'))
+
+# Create an Arrow Table
+table = pa.Table.from_arrays([arrow_timestamps, pa.array(df['ID'])], names=['timestamp', 'ID'])
 
 # Write the Table to a Parquet file
 parquet_file = 'data.parquet'
 pq.write_table(table, parquet_file)
 
-print(f"Parquet file '{parquet_file}' created.")
+print(f"Parquet file '{parquet_file}' created with {num_rows} rows.")
