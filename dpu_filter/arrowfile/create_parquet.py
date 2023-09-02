ef0604d8f975a5d8bc0123bb2ec59c2ec41deb27
Nara-Batsoyol <Nara.Batsoyol@ibm.com>
2023-09-02 22:20:10 +0000

    Update create parquet from 100 to 1Billion rows

--- dpu_filter/arrowfile/create_parquet.py
+++ dpu_filter/arrowfile/create_parquet.py
@@ -2,24 +2,51 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 
-# Generate sample data with 6000 rows
-num_rows = 200
-data = {
-    'timestamp': pd.date_range(start='2023-08-01', periods=num_rows, freq='D'),
-    'ID': range(101, 101 + num_rows)
-}
+def create_parquet_file(num_rows):
+    # Generate sample data with the specified number of rows
+    data = {
+        'timestamp': pd.date_range(start='2023-08-01', periods=num_rows, freq='D'),
+        'ID': range(101, 101 + num_rows)
+    }
 
-# Create a Pandas DataFrame
-df = pd.DataFrame(data)
+    # Create a Pandas DataFrame
+    df = pd.DataFrame(data)
 
-# Convert timestamp to Arrow timestamp data type
-arrow_timestamps = pa.array(df['timestamp'], type=pa.timestamp('s'))
+    # Convert timestamp to Arrow timestamp data type
+    arrow_timestamps = pa.array(df['timestamp'], type=pa.timestamp('s'))
 
-# Create an Arrow Table
-table = pa.Table.from_arrays([arrow_timestamps, pa.array(df['ID'])], names=['timestamp', 'ID'])
+    # Create an Arrow Table
+    table = pa.Table.from_arrays([arrow_timestamps, pa.array(df['ID'])], names=['timestamp', 'ID'])
 
-# Write the Table to a Parquet file
-parquet_file = 'data.parquet'
-pq.write_table(table, parquet_file)
+    # Build the Parquet filename based on the number of rows
+    parquet_file = f'data_{num_rows}.parquet'
 
-print(f"Parquet file '{parquet_file}' created with {num_rows} rows.")
+    # Write the Table to a Parquet file
+    pq.write_table(table, parquet_file)
+
+    print(f"Parquet file '{parquet_file}' created with {num_rows} rows.")
+
+# Specify the number of rows
+num_rows = 100
+create_parquet_file(num_rows)
+# Specify the number of rows
+num_rows = 1000
+create_parquet_file(num_rows)
+# Specify the number of rows
+num_rows = 10000
+create_parquet_file(num_rows)
+# Specify the number of rows
+num_rows = 100000
+create_parquet_file(num_rows)
+# Specify the number of rows
+num_rows = 1000000
+create_parquet_file(num_rows)
+# Specify the number of rows
+num_rows = 10000000
+create_parquet_file(num_rows)
+# Specify the number of rows
+num_rows = 100000000
+create_parquet_file(num_rows)
+# Specify the number of rows
+num_rows = 1000000000
+create_parquet_file(num_rows)
