b7ec530e8747ecaf8b72d522c91c1550af534722
Nara-Batsoyol <Nara.Batsoyol@ibm.com>
2023-09-02 20:53:45 +0000

    Update parquet read script

--- dpu_filter/arrowfile/read_parquet.py
+++ dpu_filter/arrowfile/read_parquet.py
@@ -1,13 +1,22 @@
+import sys
+import pandas as pd
+import pyarrow as pa
 import pyarrow.parquet as pq
 
-# Path to the Parquet file
-parquet_file = 'data.parquet'
+def read_and_display_parquet(parquet_file):
+    # Read the Parquet file
+    table = pq.read_table(parquet_file)
 
-# Open the Parquet file
-table = pq.read_table(parquet_file)
+    # Convert the Arrow Table to a Pandas DataFrame
+    df = table.to_pandas()
 
-# Convert the table to a Pandas DataFrame
-df = table.to_pandas()
+    # Display the DataFrame
+    print("DataFrame:")
+    print(df)
 
-# Print the DataFrame
-print(df)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py parquet_file")
+    else:
+        parquet_file = sys.argv[1]
+        read_and_display_parquet(parquet_file)
