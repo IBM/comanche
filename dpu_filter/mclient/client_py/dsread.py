510a10aedc98cc648838bf9c31b2148752c4c81d
Narangerelt <nbatsoyo@ucsd.edu>
2024-05-21 15:46:34 -0700

    client py

--- dpu_filter/mclient/client_py/dsread.py
+++ dpu_filter/mclient/client_py/dsread.py
@@ -1,18 +1,18 @@
-import pyarrow.parquet as pq
 import s3fs
 import time
+import pyarrow.dataset as ds
+import pandas as pd  # Ensure pandas is imported
+
 # Time measurement for full execution
 start_time = time.time()
 
-# Time measurement for download
-download_start_time = time.time()
 # Configure s3fs to use your MinIO instance
 fs = s3fs.S3FileSystem(
     key='minioadmin',  # MinIO access key
     secret='minioadmin',  # MinIO secret key
     client_kwargs={
         'endpoint_url': 'https://10.10.10.18:9000',  # MinIO endpoint URL
-        'verify': '/usr/local/share/ca-certificates/public.crt'  # If your MinIO server uses self-signed certificates, you might need this
+        'verify': '/usr/local/share/ca-certificates/public.crt'  # Path to your CA certificate
     },
     config_kwargs={
         's3': {
@@ -21,34 +21,26 @@ fs = s3fs.S3FileSystem(
     }
 )
 
-# Time measurement for download
-download_end_time = time.time()
-
-# Calculate the download time
-download_time = download_end_time - download_start_time
-print("Download time:", download_time, "seconds")
-
-# Time measurement for converting Parquet data to Pandas DataFrame
-conversion_start_time = time.time()
 # Construct the S3 path to your Parquet file on MinIO
 s3_path = 's3://mycsvbucket/sampledata/dataStat_1000000.parquet'
 
-# PyArrow automatically handles efficient data access
-dataset = pq.ParquetDataset(s3_path, filesystem=fs)
+# Time measurement for converting Parquet data to Pandas DataFrame
+conversion_start_time = time.time()
 
-table = dataset.read()  # Replace 'column1', 'column2' with actual column names
+# Load the dataset with a filter
+dataset = ds.dataset(s3_path, filesystem=fs, format="parquet")
 conversion_end_time = time.time()
+
+table = dataset.to_table(filter=ds.field('ID') < 120)
 
 
 # Calculate the conversion time
 conversion_time = conversion_end_time - conversion_start_time
 print("Conversion time:", conversion_time, "seconds")
 
-# Now you can work with 'table' as needed, e.g., convert to Pandas DataFrame
+# Convert to Pandas DataFrame
 df = table.to_pandas()
-result = df.query('ID <120')
-print(result)
-
+print(df)
 
 # Time measurement for full execution
 end_time = time.time()
